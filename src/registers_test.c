785b8f3116cc3598e4e749db4f02f666af458747
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-12-16 21:07:19 +0100

    Write tests for create and get_mode

--- src/registers_test.c
+++ src/registers_test.c
@@ -16,9 +16,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
 �tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 B�timent IMAG
-	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'H�res
+   B�timent IMAG
+   700 avenue centrale, domaine universitaire
+   38401 Saint Martin d'H�res
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,52 +28,123 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_constants.h"
 #include "util.h"
 
-void print_test(int result) {
-    if (result)
-        printf("Test succeded\n");
-    else
-        printf("TEST FAILED !!\n");
+void print_test(int result)
+{
+  if (result)
+    printf("Test succeded\n");
+  else
+    printf("TEST FAILED !!\n");
 }
 
-int main() {
-    registers r;
-    uint32_t word_value[15], word_read;
-    int i;
+void test_registers_create()
+{
+  printf("Test : Create registers ... ");
+  registers r = registers_create();
+  assert(r != NULL);
+  printf("OK\n");
+  registers_destroy(r);
+}
 
-    r = registers_create();
-    if (r == NULL) {
-        fprintf(stderr, "Error when creating simulated registers\n");
-        exit(1);
-    }
-    srandom(getpid());
-    for (i = 0; i < 15; i++) {
-        word_value[i] = random();
-    }
+void test_registers_get_mode()
+{
+  registers r = registers_create();
+  assert(r != NULL);
 
-    // Arm reset
-    printf("Reseting registers...\n");
-    registers_write_cpsr(r, 0x1d3);
-    uint8_t mode = registers_get_mode(r);
-    registers_write(r, 15, mode, 0);
+  printf("Test : Get mode (USR) ... ");
+  // 0x110 = 0000 0001 0001 0000 = Mode USR
+  registers_write_cpsr(r, 0x110);
+  assert(registers_get_mode(r) == USR);
+  printf("OK\n");
 
-    printf("Testing read and write to general registers:\n");
-    for (i = 0; i < 15; i++) {
-        registers_write(r, i, mode, word_value[i]);
-    }
-    for (i = 0; i < 15; i++) {
-        word_read = registers_read(r, i, mode);
-        printf("- register %d : ", i);
-        print_test(word_read == word_value[i]);
-    }
+  printf("Test : Get mode (FIQ) ... ");
+  // 0x11 = 0000 0000 0001 0001 = Mode FIQ
+  registers_write_cpsr(r, 0x11);
+  assert(registers_get_mode(r) == FIQ);
+  printf("OK\n");
 
-    printf("Current mode : ");
-    print_test(registers_get_mode(r) == SVC);
-    printf("Mode is priviledged : ");
-    print_test(registers_in_a_privileged_mode(r));
-    printf("Mode has spsr : ");
-    print_test(registers_current_mode_has_spsr(r));
+  printf("Test : Get mode (IRQ) ... ");
+  // 0x12 = 0000 0000 0001 0010 = Mode IRQ
+  registers_write_cpsr(r, 0x12);
+  assert(registers_get_mode(r) == IRQ);
+  printf("OK\n");
 
-    registers_destroy(r);
+  printf("Test : Get mode (SVC) ... ");
+  // 0x13 = 0000 0000 0001 0011 = Mode SVC
+  registers_write_cpsr(r, 0x13);
+  assert(registers_get_mode(r) == SVC);
+  printf("OK\n");
 
-    return 0;
+  printf("Test : Get mode (ABT) ... ");
+  // 0x17 = 0000 0000 0001 0111 = Mode ABT
+  registers_write_cpsr(r, 0x17);
+  assert(registers_get_mode(r) == ABT);
+  printf("OK\n");
+
+  printf("Test : Get mode (UND) ... ");
+  // 0x1b = 0000 0000 0001 1011 = Mode UND
+  registers_write_cpsr(r, 0x1b);
+  assert(registers_get_mode(r) == UND);
+  printf("OK\n");
+
+  printf("Test : Get mode (SYS) ... ");
+  // 0x1f = 0000 0000 0001 1111 = Mode SYS
+  registers_write_cpsr(r, 0x1f);
+  assert(registers_get_mode(r) == SYS);
+  printf("OK\n");
+
+  registers_destroy(r);
+}
+
+int main()
+{
+
+  test_registers_create();
+  test_registers_get_mode();
+
+  return 0;
+
+  registers r;
+  uint32_t word_value[15], word_read;
+  int i;
+
+  r = registers_create();
+  if (r == NULL)
+  {
+    fprintf(stderr, "Error when creating simulated registers\n");
+    exit(1);
+  }
+  srandom(getpid());
+  for (i = 0; i < 15; i++)
+  {
+    word_value[i] = random();
+  }
+
+  // Arm reset
+  printf("Reseting registers...\n");
+  registers_write_cpsr(r, 0x1d3);
+  uint8_t mode = registers_get_mode(r);
+  registers_write(r, 15, mode, 0);
+
+  printf("Testing read and write to general registers:\n");
+  for (i = 0; i < 15; i++)
+  {
+    registers_write(r, i, mode, word_value[i]);
+  }
+  for (i = 0; i < 15; i++)
+  {
+    word_read = registers_read(r, i, mode);
+    printf("- register %d : ", i);
+    print_test(word_read == word_value[i]);
+  }
+
+  printf("Current mode : ");
+  print_test(registers_get_mode(r) == SVC);
+  printf("Mode is priviledged : ");
+  print_test(registers_in_a_privileged_mode(r));
+  printf("Mode has spsr : ");
+  print_test(registers_current_mode_has_spsr(r));
+
+  registers_destroy(r);
+
+  return 0;
 }
