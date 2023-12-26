458d99cdc0e586162def74f33c6418ecf3b05875
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-12-26 13:45:34 +0100

    Add a new test

--- src/test_arm_data_processing.c
+++ src/test_arm_data_processing.c
@@ -16,6 +16,15 @@ void test_add(arm_core p)
   arm_data_processing_add(p, 0b11100010100000000001000000000011);
   assert(registers_read(p->reg, 1, USR) == 5);
   printf("OK\n");
+
+  printf("Test : ADD (Unvalid condition) ... ");
+  registers_write(p->reg, 0, USR, 2);
+  registers_write_Z(p->reg, 0);
+  // Cond -- I ---- S Rn   Rd   Shifter
+  // 0000 00 1 0100 0 0000 0000 000000000011
+  arm_data_processing_add(p, 0b00000010100000000000000000000011);
+  assert(registers_read(p->reg, 0, USR) == 2);
+  printf("OK\n");
 }
 
 int main()
