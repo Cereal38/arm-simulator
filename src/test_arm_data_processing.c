828305cefee4baad9db40bdb782af83664578943
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-12-26 13:38:38 +0100

    Fix minor bug

--- src/test_arm_data_processing.c
+++ src/test_arm_data_processing.c
@@ -11,12 +11,9 @@ void test_add(arm_core p)
 {
   printf("Test : ADD (2 + 3) ... ");
   registers_write(p->reg, 0, USR, 2);
-  printf("\n%d\n", registers_read(p->reg, 0, USR));
   // Cond -- I ---- S Rn   Rd   Shifter
   // 1110 00 1 0100 0 0000 0001 000000000011
-  arm_data_processing_add(p, 0b1110001010000000000100000011);
-  printf("\n%d\n", registers_read(p->reg, 0, USR));
-  printf("\n%d\n", registers_read(p->reg, 1, USR));
+  arm_data_processing_add(p, 0b11100010100000000001000000000011);
   assert(registers_read(p->reg, 1, USR) == 5);
   printf("OK\n");
 }
