60737962d576760cc5e943ec0c75bcbf990b7a9a
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-12-26 13:16:07 +0100

    Create the p in the test file

--- src/test_arm_data_processing.c
+++ src/test_arm_data_processing.c
@@ -1,10 +1,19 @@
 
 #include <stdio.h>
+#include "arm_data_processing.h"
+#include "arm_core.h"
+#include "registers.h"
+#include "memory.h"
+
+void test_add(arm_core p)
+{
+  arm_data_processing_immediate_msr(p, 0);
+}
 
 int main()
 {
-
-  printf("Test\n");
+  arm_core p = arm_create(registers_create(), memory_create(2048));
+  test_add(p);
 
   return 0;
 }
