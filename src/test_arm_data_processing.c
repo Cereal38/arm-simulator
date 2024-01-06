027d642c5e0bcac8b9b7addc46ec6cdf3dfeb061
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-06 13:53:50 +0100

    Test SBC

--- src/test_arm_data_processing.c
+++ src/test_arm_data_processing.c
@@ -447,6 +447,69 @@ void test_adc(arm_core p)
       0);             // Expected V flag
 }
 
+void test_sbc(arm_core p)
+{
+  // 4 - 2 - 1 = 1
+  registers_write_C(p->reg, 0);
+  test_template(
+      "SBC (Immediate value)",
+      p,
+      AL,             // Cond
+      1,              // I : Immediate value
+      SBC,            // Opcode
+      1,              // S : Set condition codes
+      1,              // Rn : r1
+      0,              // Rd : r0
+      0b000000000010, // Shifter : 2
+      4,              // Rn value
+      0,              // Rs value
+      1,              // Expected Rd value
+      0,              // Expected Z flag
+      0,              // Expected N flag
+      1,              // Expected C flag
+      0);             // Expected V flag
+
+  // 4 - 2 - 0 = 2
+  registers_write_C(p->reg, 1);
+  test_template(
+      "SBC (With borrow)",
+      p,
+      AL,             // Cond
+      1,              // I : Immediate value
+      SBC,            // Opcode
+      1,              // S : Set condition codes
+      1,              // Rn : r1
+      0,              // Rd : r0
+      0b000000000010, // Shifter : 2
+      4,              // Rn value
+      0,              // Rs value
+      2,              // Expected Rd value
+      0,              // Expected Z flag
+      0,              // Expected N flag
+      1,              // Expected C flag
+      0);             // Expected V flag
+
+  // 0x80000001 - 0x1 - 1 = 0x7FFFFFFF
+  registers_write_C(p->reg, 0);
+  test_template(
+      "SBC (Overflow)",
+      p,
+      AL,             // Cond
+      1,              // I : Immediate value
+      SBC,            // Opcode
+      1,              // S : Set condition codes
+      1,              // Rn : r1
+      0,              // Rd : r0
+      0b000000000001, // Shifter
+      0x80000001,     // Rn value
+      0,              // Rs value
+      0x7FFFFFFF,     // Expected Rd value
+      0,              // Expected Z flag
+      0,              // Expected N flag
+      1,              // Expected C flag
+      1);             // Expected V flag
+}
+
 int main()
 {
   arm_core p = arm_create(registers_create(), memory_create(2048));
@@ -457,6 +520,7 @@ int main()
   test_eor(p);
   test_rsb(p);
   test_adc(p);
+  test_sbc(p);
 
   memory_destroy(p->mem);
   registers_destroy(p->reg);
