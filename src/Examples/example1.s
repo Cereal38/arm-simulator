4c4a41d1f54688349bc96b8243c5acd09014ac23
root <root@LAPTOP-3O5NS466>
2024-01-11 11:29:38 +0100

    modification mrs msr

--- src/Examples/example1.s
+++ src/Examples/example1.s
@@ -1,27 +1,9 @@
  .global main
 .text
 main:
-    
-    MOV r1,#7
-    MOV r2,#8
-branch:
-    MOV r3,#2
-    ADD r1,r1,r2            
-    ADD r1,r1,r2            
-    MOV r3, #0
-    B branch
-    MOV r3,#0
-    MOV r1, r14
-    ADC r3,r1,r2,LSL #2
-    ADC r4,r1,r2,LSR #2
-    ADC r5,r1,r2,ROR #2
-    ADC r6,r1,r2,ASL #2
-    ADC r7,r1,r2,ASR #2
-    ADCS r3,r1,r2,LSL #2
-    ADCS r4,r1,r2,LSR #2
-    ADCS r5,r1,r2,ROR #2
-    ADCS r6,r1,r2,ASL #2
-    ADCS r7,r1,r2,ASR #2
+    MRS R0, CPSR 
+	ORR R0, R0, #0x80 
+	MSR CPSR_c, R0
     swi 0x123456
 .data
 limite:
