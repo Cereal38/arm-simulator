4a7dab2073f29e985ce1a7bf6c7e952e7e469ce7
AntoineDesfonds <152633749+AntoineDesfonds@users.noreply.github.com>
2024-01-11 06:00:32 +0100

    test STM LDM

--- src/test_arm_load_store.c
+++ src/test_arm_load_store.c
@@ -28,9 +28,7 @@ void test_template_load_store(
     // Set instruction
     uint32_t ins = (cond << 28) | (code << 20) | (Rn << 16) | (Rd << 12) | addr;
     // Execute
-    //printf("Before execution, Rn = %u\n", registers_read(p->reg, Rn, USR));
     arm_load_store(p, ins);
-    //printf("After execution, Rd = %u\n", registers_read(p->reg, Rd, USR));
     // Check result
     assert(registers_read(p->reg, Rd, USR) == expected_Rd);
 }
@@ -38,43 +36,71 @@ void test_template_load_store(
 void test_template_load_store_multiple(
     char *name,
     arm_core p,
-    uint8_t cond,  // Bits 31-28
-    uint8_t code,  // Bits 27-20
-    uint8_t Rn,    // Bits 19-16
-    uint16_t register_list,//Bits 15-0
+    uint8_t cond,           // Bits 31-28
+    uint8_t code,           // Bits 27-20
+    uint8_t Rn,             // Bits 19-16
+    uint16_t register_list, // Bits 15-0
+    uint32_t Rn_value,
+    int incr,
     uint32_t expected_value)
 {
-    printf("Test : %s ... \n", name);
+    printf("Test : %s ... ", name);
+
+    //Set Rn
+    arm_write_register(p, Rn, Rn_value);
+
     // Set instruction
     uint32_t ins = (cond << 28) | (code << 20) | (Rn << 16) | register_list;
+
     // Execute
-    printf("Before execution, Rn = %u\n", registers_read(p->reg, Rn, registers_get_mode(p->reg)));
-    printf("Valeur avant r0: %d \n", registers_read(p->reg, 0, registers_get_mode(p->reg)));
-    printf("Valeur avant r1: %d \n", registers_read(p->reg, 1, registers_get_mode(p->reg)));
     arm_load_store_multiple(p, ins);
-    printf("After execution, Rn = %u\n", registers_read(p->reg, Rn, registers_get_mode(p->reg)));
-    printf("Valeur après r0: %d \n", registers_read(p->reg, 0, registers_get_mode(p->reg)));
-    printf("Valeur après r1: %d \n", registers_read(p->reg, 1, registers_get_mode(p->reg)));
+
     // Check result
-    assert(registers_read(p->reg, 0, registers_get_mode(p->reg)) == expected_value);
+    uint32_t addr = registers_read(p->reg, Rn, registers_get_mode(p->reg));
+    for (int i = 0; i < 16; i++){
+        if ((register_list & (1 << i)) != 0){
+            uint32_t word;
+            arm_read_word(p, addr, &word);
+            assert(word == expected_value);
+            addr += incr;
+        }
+    }
+    printf("OK\n");
 }
 
-void test_LDM(arm_core p){
+void test_STM(arm_core p){
 
+    //TEST 1
     uint8_t Rn = 0; // r0;
-    uint16_t register_list = 0b10; // r1
+    uint16_t register_list = 0b0000000000000010; // r1
+    arm_write_word(p, 1, 2);//r1 = 2
+    uint32_t expected_value = 2;//r0 doit être égal à 2 (comme r1)
 
-    registers_write(p->reg, 1, registers_get_mode(p->reg), 2);//r1 = 2
-
-    uint32_t expected_value = 2;
-
-    printf("Valeur initiale de RN : %d\n", Rn);
-    test_template_load_store_multiple("LDM incremente AFTER 1 element, r0 dans r1\n",
+    test_template_load_store_multiple("STM 1 element, r0 <- r1",
                                       p,
                                       AL,
-                                      0b10001011,
+                                      0b10001000,
                                       Rn,
                                       register_list,
+                                      0,
+                                      4,
+                                      expected_value);
+
+    //TEST 2 (ce test ne passe pas)
+    Rn = 0; // r0;
+    register_list = 0b0000000000001100; // r2 r3
+    arm_write_word(p, 2, 3);//r2 = 3
+    arm_write_word(p, 3, 3);//r3 = 3
+    expected_value = 3;//r0 et r1 doivent être égal à 3 (comme r2 et r3)
+
+    test_template_load_store_multiple("STM 2 elements, r0 <- r2, r1 <- r3",
+                                      p,
+                                      AL,
+                                      0b10001000,
+                                      Rn,
+                                      register_list,
+                                      0,
+                                      4,
                                       expected_value);
 }
 
@@ -131,7 +157,7 @@ void test_LDR(arm_core p)
 
 int main(){
     arm_core p = arm_create(registers_create(), memory_create(2048));
-    test_LDM(p);
+    test_STM(p);
     memory_destroy(p->mem);
     registers_destroy(p->reg);
     arm_destroy(p);
